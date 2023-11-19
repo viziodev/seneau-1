f9185ed4ec318c5d5c04fa97682ede5c3313662e
Mouhamed <ndoyeahmed2602@gmail.com>
2023-11-19 22:15:12 +0000

    workflow demande

---
+++ demande-service/src/main/java/com/seneau/demandeservice/DemandeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.seneau.demandeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemandeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemandeServiceApplication.class, args);
+    }
+
+}
---
+++ demande-service/src/test/java/com/seneau/demandeservice/DemandeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seneau.demandeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemandeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
